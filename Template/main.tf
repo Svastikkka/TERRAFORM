1845f0d014cfbcadacea7d354e5c01e36dcba13b
Manshu Sharma <manshu.sharma@digite.com>
2023-03-21 13:17:41 +0530

    update: ec2 template resource

--- Template/main.tf
+++ Template/main.tf
@@ -1,92 +1,27 @@
-resource "aws_launch_template" "sample" {
-  name = "sample"
+resource "aws_launch_template" "your_eks_launch_template" {
+  name = "fabric-dev-template"
+
+  # vpc_security_group_ids = [var.your_security_group.id, aws_eks_cluster.your-eks-cluster.vpc_config[0].cluster_security_group_id]
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 20
+      volume_type = "gp3"
+    }
+  }
+
   image_id = "ami-0ebf01230aca0c60f"
-
-#   block_device_mappings {
-#     device_name = "/dev/sdf"
-
-#     ebs {
-#       volume_size = 20
-#     }
-#   }
-
-#   capacity_reservation_specification {
-#     capacity_reservation_preference = "open"
-#   }
-
-#   cpu_options {
-#     core_count       = 4
-#     threads_per_core = 2
-#   }
-
-#   credit_specification {
-#     cpu_credits = "standard"
-#   }
-
-#   disable_api_stop        = true
-#   disable_api_termination = true
-
-#   ebs_optimized = true
-
-#   elastic_gpu_specifications {
-#     type = "test"
-#   }
-
-#   elastic_inference_accelerator {
-#     type = "eia1.medium"
-#   }
-
-#   iam_instance_profile {
-#     name = "test"
-#   }
-
-#   instance_initiated_shutdown_behavior = "terminate"
-
-#   instance_market_options {
-#     market_type = "spot"
-#   }
-
-#   instance_type = "t2.micro"
-
-#   kernel_id = "test"
-
-#   key_name = "test"
-
-#   license_specification {
-#     license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
-#   }
-
-#   metadata_options {
-#     http_endpoint               = "enabled"
-#     http_tokens                 = "required"
-#     http_put_response_hop_limit = 1
-#     instance_metadata_tags      = "enabled"
-#   }
-
-#   monitoring {
-#     enabled = true
-#   }
-
-#   network_interfaces {
-#     associate_public_ip_address = true
-#   }
-
-#   placement {
-#     availability_zone = "us-west-2a"
-#   }
-
-#   ram_disk_id = "test"
-
-#   vpc_security_group_ids = ["sg-12345678"]
+  instance_type = "t3.large"
+  # user_data = ""
 
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name = "sample"
-      Cost = "Swiftalk"
+      Name = "fabric-dev"
+      Environment = "dev"
     }
   }
-
-  # user_data = filebase64("${path.module}/example.sh")
 }
